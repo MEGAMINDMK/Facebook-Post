e5af1f635430fb7769f80698b8f375c87a335d84
webnet <muhommadraahim1@gmail.com>
2019-09-15 17:57:42 +0500

    Add files via upload

---
+++ fbpost_.php
@@ -0,0 +1,14 @@
+<form action="" method="get">
+
+Enter Your Message:<input type="text" name="msg" placeholder="message" autocomplete="off" required><br>
+Enter Link:<input type="text" name="link" placeholder="link" autocomplete="off" required><br>
+
+<input type="submit" value="ok">
+</form>
+<?php
+error_reporting(0);
+if (!empty($m = $_GET["msg"])&&($l = $_GET['link'])) {
+    echo "<a href='https://www.facebook.com/sharer/sharer.php?u=".$l."&quote=".$m."' target='_blank'>Post</a>";    
+}
+?>
+
